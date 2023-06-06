edfefb5dac129141bedd61bce46e620c4365835c
davidbachii <93995663+davidbachii@users.noreply.github.com>
2023-06-06 16:31:11 +0200

    Add files via upload

---
+++ prac3b/include/leon3_bprint.h
@@ -0,0 +1,16 @@
+/*
+ * leon3_bprint.h
+ *
+ *  Created on: Feb 28, 2023
+ *      Author: alex
+ */
+
+#include "leon3_types.h"
+#ifndef LEON3_BPRINT_H_
+#define LEON3_BPRINT_H_
+
+int8_t leon3_print_uint32( uint32_t i);
+
+int8_t leon3_print_string(char* str);
+
+#endif /* LEON3_BPRINT_H_ */
---
+++ prac3b/include/leon3_uart.h
@@ -0,0 +1,24 @@
+/*
+ * leon3_uart.h
+ *
+ *  Created on: Feb 18, 2022
+ *      Author: alex
+ */
+
+#ifndef LEON3_UART_H_
+#define LEON3_UART_H_
+
+#include "leon3_types.h"
+
+int8_t leon3_putchar(char c);
+
+int8_t leon3_uart_tx_fifo_is_empty();
+
+
+char leon3_getchar();
+void leon3_uart_ctrl_rx_enable();
+void leon3_uart_ctrl_rx_irq_enable();
+void leon3_uart_ctrl_config_rxtx_loop(uint8_t set_rxtxloop);
+
+
+#endif /* LEON3_UART_H_ */
